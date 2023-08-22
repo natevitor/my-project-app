f1baa891cbfea620a733ae07db2e544046e24779
Logon Aluno <rm550381@fiap.com.br>
2023-08-22 10:03:24 -0300

    #

---
+++ components/Cabecalho.jsx
@@ -0,0 +1,9 @@
+export default function Cabecalho(){
+    return (
+        <>
+          <header>
+             <h1> First DEPLOY in to VERCEL</h1>
+          </header>
+        </>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import Cabecalho from "../components/Cabecalho"
 
 export default function App() {
   return (
     <>
+    <div className="container">
 
-    <h1>First DEPLOY in to VERCEL</h1>
-    <h2>Vite + React</h2>
-    <p>Coded By - RM550381</p>
+      <Cabecalho/>
 
+      <header>
+        <h1>First DEPLOY in to VERCEL</h1>
+      </header>
 
+      <section>
+        <h2>Vite + React</h2>
+        <p>Coded By - RM550381</p>
+        <div>
+          <p>Turma -1tdspl</p>
+          <p>Fiap</p>
+       </div>
+      </section>
+
+      <footer>
+        <a href="GitHub"></a>
+        <a href="GitHub"></a>
+        <a href="GitHub"></a>
+      </footer>
+
+    </div>
     </>
 
   )

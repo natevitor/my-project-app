0646c5e434f487830d9a55b50388469c40aa5a24
Logon Aluno <rm550381@fiap.com.br>
2023-08-22 09:34:45 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+
+    <h1>First DEPLOY in to VERCEL</h1>
+    <h2>Vite + React</h2>
+    <p>Coded By - RM550381</p>
+
+
+    </>
+
+  )
+}
